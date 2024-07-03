eb7784f6b9f1f655369a6c5a5d88ada02b97e8a0
Liam Mulhall <liammulh@gmail.com>
2024-07-03 14:54:59 -0700

    Rename Django project

--- src/main/asgi.py
+++ src/main/asgi.py
@@ -10,6 +10,6 @@ import os
 # Third-party dependencies:
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hci.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
 
 application = get_asgi_application()
