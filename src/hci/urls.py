dd9281f0bf16a967a14712582aaef273c5134cb8
Liam Mulhall <liammulh@gmail.com>
2024-07-24 16:45:07 -0700

    Make user log in before they can view the home page

--- src/hci/urls.py
+++ src/hci/urls.py
@@ -1,11 +1,12 @@
 """URL patterns for the HCI."""
 
 # Third-party dependencies:
-from django.urls import path
+from django.urls import path, include
 
 # In-house code:
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
--- src/hci/views.py
+++ src/hci/views.py
@@ -2,8 +2,10 @@
 
 # Third-party dependencies:
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def index(request):
     """Show basic info at the index route."""
     return render(request, "hci/index.html")
