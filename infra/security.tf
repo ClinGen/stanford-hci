787b881532aa7f46616cb0b77418f0a185ad18e0
Liam Mulhall <liammulh@gmail.com>
2025-01-28 11:32:44 -0800

    Add security groups

---
+++ infra/security.tf
@@ -0,0 +1,55 @@
+// This module configures the security groups for the HCI.
+
+// Configure the application load balancer (ALB) security group.
+// (Internet -> ALB)
+resource "aws_security_group" "hci_load_balancer" {
+  name        = "hci_load_balancer_security_group_${terraform.workspace}"
+  description = "This security group controls access to the ALB."
+  vpc_id      = aws_vpc.hci_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+// Configure the ECS Fargate security group.
+// (ALB -> Fargate)
+resource "aws_security_group" "hci_ecs_fargate" {
+  name        = "hci_ecs_fargate_security_group_${terraform.workspace}"
+  description = "This security group allows inbound access from the ALB."
+  vpc_id      = aws_vpc.hci_vpc.id
+
+  ingress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    security_groups = [aws_security_group.hci_load_balancer.id]
+  }
+
+  // There is no SSH ingress rule since Fargate tasks are abstracted and not
+  // directly accessible via SSH.
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
