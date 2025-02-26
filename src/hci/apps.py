fadcc5a7bab3fc9ae6a3c1245f5510ab7fa88a43
Liam Mulhall <liammulh@gmail.com>
2025-02-26 15:01:37 -0800

    Remove extra templates dir

--- src/hci/apps.py
+++ src/hci/apps.py
@@ -1,30 +1,9 @@
 """This module contains the Django configuration of the HCI app."""
 
-import os
-
 from django.apps import AppConfig
-from django.contrib.auth.models import User
-from django.core.exceptions import ObjectDoesNotExist
-
-from util.secrets import get_secret
 
 
 class HCIConfig(AppConfig):
     """Configure the HCI Django app."""
 
     name = "hci"
-
-    def ready(self):
-        """Run code when Django starts."""
-        # Make sure a superuser exists.
-        host = os.getenv("HCI_HOST")
-        if host in ("staging", "production"):
-            username = get_secret(f"hci_django_superuser_username_{host}")
-            try:
-                User.objects.get(username=username)
-            except ObjectDoesNotExist:
-                User.objects.create_superuser(
-                    username=username,
-                    email=get_secret(f"hci_django_superuser_email_{host}"),
-                    password=get_secret(f"hci_django_superuser_password_{host}"),
-                )
