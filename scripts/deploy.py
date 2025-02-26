9416d07ee930a80dfb18074de36373e026b577e1
Liam Mulhall <liammulh@gmail.com>
2025-02-26 11:37:07 -0800

    Add prompts to deploy script

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -19,16 +19,17 @@ import sys
 import click
 
 
-def run(cmd: str, silent: bool = False):
+def run(cmd: str):
     """Runs a command and prints a friendly error message if necessary."""
     try:
-        if not silent:
-            print(f"Running command: {cmd}")
+        print(f"Running command: {cmd}")
         result = subprocess.run(
             cmd.split(" "), capture_output=True, check=True, text=True
         )
-        if result.returncode == 0 and not silent:
+        if result.returncode == 0:
             print("Done")
+        else:
+            print(f"Done with return code {result.returncode}")
     except subprocess.CalledProcessError as err:
         print(f"Error: Command {cmd} failed with return code {err.returncode}")
         if err.stdout:
@@ -45,12 +46,7 @@ def run(cmd: str, silent: bool = False):
     help="Where to deploy the HCI (either 'staging' or 'production')",
     required=True,
 )
-@click.option(
-    "--silent",
-    help="Whether to prompt the user for a yes/no response about deploying (either 'true' or 'false')",
-    required=False,
-)
-def deploy(environment: str, silent: str):
+def deploy(environment: str):
     """Deploys the HCI to the given environment."""
 
     if environment not in ("staging", "production"):
@@ -58,28 +54,44 @@ def deploy(environment: str, silent: str):
         print("Error: Environment should be either 'staging' or 'production'")
         sys.exit(1)
 
-    if not silent or silent.lower() in ("false", "f", "no", "n"):
-        ans = input(
-            f"This will deploy the HCI to the {environment} environment. Do you wish to proceed? (y/n): "
-        )
-        if ans.lower() in ("no", "n"):
-            sys.exit(1)
+    ans = input(
+        f"This will deploy the HCI to the {environment} environment. Do you wish to proceed? (y/n): "
+    )
+    if ans.lower() != "y":
+        sys.exit(1)
 
-    # Set up commands.
-    if environment == "staging":
-        env_abbrev = "stag"
-    else:
+    # We need the abbreviation for our run script commands.
+    if environment == "production":
         env_abbrev = "prod"
-    migrate_db = f"./run db:migrate:{env_abbrev}"
-    print(migrate_db)
-    authenticate_to_ecr = "./run ecr:auth"
-    print(authenticate_to_ecr)
-    build_new_docker_image = f"./run ecr:build:{env_abbrev}"
-    print(build_new_docker_image)
-    push_image_to_ecr = f"./run ecr:push:{env_abbrev}"
-    print(push_image_to_ecr)
-    update_ecs = f"./run ecs:update:{env_abbrev}"
-    print(update_ecs)
+    else:
+        env_abbrev = "stag"
+
+    ans = input(f"Migrate the {environment} database? (y/n): ")
+    if ans.lower() == "y":
+        migrate_db = f"./run db:migrate:{env_abbrev}"
+        run(migrate_db)
+
+    ans = input(
+        "Authenticate to ECR? This might be necessary if you haven't done it recently.) (y/n): "
+    )
+    if ans.lower() == "y":
+        authenticate_to_ecr = "./run ecr:auth"
+        run(authenticate_to_ecr)
+
+    ans = input("Build new container image? (y/n): ")
+    if ans.lower() == "y":
+        build_new_docker_image = f"./run ecr:build:{env_abbrev}"
+        run(build_new_docker_image)
+
+    ans = input("Push the new container image to ECR? (y/n): ")
+    if ans.lower() == "y":
+        push_image_to_ecr = f"./run ecr:push:{env_abbrev}"
+        run(push_image_to_ecr)
+
+    ans = input("Update the ECS service? (y/n): ")
+    if ans.lower() == "y":
+        update_ecs = f"./run ecs:update:{env_abbrev}"
+        run(update_ecs)
 
 
 if __name__ == "__main__":
