c2fa3c25dc2d6bb728593ad06440c506808dfea2
Liam Mulhall <liammulh@gmail.com>
2025-04-20 15:29:43 -0700

    Use relevant article in test

--- src/apps/publications/services/pubmed_test.py
+++ src/apps/publications/services/pubmed_test.py
@@ -3,8 +3,10 @@
 from apps.publications.services.pubmed import PubMedArticleClient
 
 
-def test_get_article_info() -> None:
+def test_client() -> None:
     """Make sure we can get article info from PubMed."""
-    pmid = "37704778"
+    pmid = "10446108"
+    title = "HLA-DR and -DQ phenotypes in inflammatory bowel disease: a meta-analysis."
     article = PubMedArticleClient(pmid)
     assert isinstance(article.title, str)
+    assert article.title == title
